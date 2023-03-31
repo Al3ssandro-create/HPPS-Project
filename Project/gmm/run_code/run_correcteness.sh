004954f63209878e93a09fc6f91282ca6a2f32b1
Matteo-Negro <matteo3.negro@mail.polimi.it>
2023-03-31 14:42:32 +0000

    Add code to check the code

--- Project/gmm/run_code/run_correcteness.sh
+++ Project/gmm/run_code/run_correcteness.sh
@@ -1,1 +1,6 @@
-../gmm 16 ../data/mydata.txt ../output/out_check_new.txt 8
+../gmm 16 ../data/mydata.txt ../output/out_check.txt 8 | grep "Final rissanen Score was:" > ../output/tmp1.txt
+../gmm 16 ../data/mydata.txt ../output/out_check.txt 8 | grep "Final rissanen Score was:" > ../output/tmp2.txt
+diff ../output/tmp1.txt ../output/tmp2.txt
+rm ../output/tmp1.txt
+rm ../output/tmp2.txt
+rm ../output/out_check.*
