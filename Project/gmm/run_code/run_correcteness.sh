67ae02c1a4b49aec7eee6042cc9bb405efdba0c5
Matteo-Negro <matteo3.negro@mail.polimi.it>
2023-03-31 14:48:04 +0000

    Correctness 2.0

--- Project/gmm/run_code/run_correcteness.sh
+++ Project/gmm/run_code/run_correcteness.sh
@@ -1,6 +1,4 @@
-../gmm 16 ../data/mydata.txt ../output/out_check.txt 8 | grep "Final rissanen Score was:" > ../output/tmp1.txt
-../gmm 16 ../data/mydata.txt ../output/out_check.txt 8 | grep "Final rissanen Score was:" > ../output/tmp2.txt
-diff ../output/tmp1.txt ../output/tmp2.txt
-rm ../output/tmp1.txt
-rm ../output/tmp2.txt
-rm ../output/out_check.*
+../gmm 16 ../data/mydata.txt ../output/new_out_check.txt 8 | grep "Final rissanen Score was:" > ../output/new_res.txt
+diff ../output/old_res.txt ../output/new_res.txt
+rm ../output/new_res.txt
+rm ../output/new_out_check.*
