6e40786377ef8a2b8536b6e5a5acd12ad23b256e
Matteo-Negro <matteo3.negro@mail.polimi.it>
2023-04-06 16:39:22 +0000

    Data collection for 2 gpu

--- Project/gmm/run_code/run_2g_strong.sh
+++ Project/gmm/run_code/run_2g_strong.sh
@@ -1,2 +1,4 @@
 export CUDA_VISIBLE_DEVICES=0,1
-./gmm 32 mydata_4g.txt out.txt 8
+../gmm 32 ../data/mydata_2g.txt ../../Part_1/results/01_out_2g_strong.txt 8
+../gmm 32 ../data/mydata_2g.txt ../../Part_1/results/02_out_2g_strong.txt 8
+../gmm 32 ../data/mydata_2g.txt ../../Part_1/results/03_out_2g_strong.txt 8
