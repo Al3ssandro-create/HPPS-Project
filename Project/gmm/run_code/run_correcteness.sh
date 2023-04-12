982e2da714c4585416ca47e36423414d2dffc908
Matteo-Negro <matteo3.negro@mail.polimi.it>
2023-04-12 15:44:23 +0000

    Fixed UM porting

--- Project/gmm/run_code/run_correcteness.sh
+++ Project/gmm/run_code/run_correcteness.sh
@@ -1,4 +1,4 @@
-../gmm 16 ../data/mydata.txt ../output/new_out_check.txt 8 | grep "Final rissanen Score was:" > ../output/new_res.txt
+../gmm_UM 16 ../data/mydata.txt ../output/new_out_check.txt 8 | grep "Final rissanen Score was:" > ../output/new_res.txt
 diff ../output/new_out_check.txt.summary ../output/old_out_check.txt.summary 
 diff ../output/new_out_check.txt.results ../output/old_out_check.txt.results 
 rm ../output/new_res.txt
